b4776570badbd7387b56ce750b1636e38d883128
Jyoti Upadhyay <jyoti.upadhyay_cs22@gla.ac.in>
2026-04-17 11:44:29 +0530

    initial commit

--- src/main/java/com/app/quantitymeasurement/exception/InvalidTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.app.quantitymeasurement.exception;
-
-public class InvalidTokenException extends RuntimeException {
-	public InvalidTokenException() {
-		super("Invalid JWT Token");
-	}
-}
--- src/main/java/com/app/quantitymeasurement/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.app.quantitymeasurement.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException() {
-        super("User already exists");
-    }
-}
